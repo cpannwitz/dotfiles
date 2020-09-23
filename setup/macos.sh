861d99d078e1f97da09774cdfce02d89bbac5ad5
chris <chris@chriss-MacBook-Pro.local>
2020-09-23 10:09:50 +0200

    added macos settings

--- setup/macos.sh
+++ setup/macos.sh
@@ -11,6 +11,74 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 #  TODO: defaults write here
+# * FINDER SETTINGS
+# Show hidden files: ENABLED
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Show all file suffixes
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -int 1
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -int 1
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -int 1
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -int 1
+
+# Set sidebar icon size to small
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Disable automatic capitalization as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -int 0
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -int 0
+
+# Disable automatic period substitution as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -int 0
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -int 0
+
+# * KEYBOARD
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -int 1
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# * GENERAL
+# disable accent menu on hold
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "scale"
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -int 1
+
+# Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -int 0
+
+# * SCREENSHOTS
+# change screenshot format to jpg
+defaults write com.apple.screencapture type jpg
+
 
 for app in "Activity Monitor" \
 	"Address Book" \
@@ -31,3 +99,17 @@ for app in "Activity Monitor" \
 	killall "${app}" &> /dev/null
 done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
+
+
+
+
+
+
+defaults read com.apple.BluetoothAudioAgent "Apple Bitpool Max (editable)"
+defaults read com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)"
+defaults read com.apple.BluetoothAudioAgent "Apple Initial Bitpool (editable)"
+defaults read com.apple.BluetoothAudioAgent "Negotiated Bitpool"
+defaults read com.apple.BluetoothAudioAgent "Negotiated Bitpool Max"
+defaults read com.apple.BluetoothAudioAgent "Negotiated Bitpool Min"
+defaults read com.apple.BluetoothAudioAgent "Stream – Max Outstanding Packets (editable)"
+defaults read com.apple.BluetoothAudioAgent "Stream – Flush Ring on Packet Drop (editable)"
